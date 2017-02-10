171edfba3b36fbd6d0947466c51e053a63bad0ba
Hanh Tran <tranduchanh.ms@gmail.com>
2017-02-10 15:09:11 +0900

    Update main process witn new window config

--- main.js
+++ main.js
@@ -1,15 +1,51 @@
 'use strict';
+const url = require('url');
+const path = require('path');
+let { app, BrowserWindow } = require('electron');
+let mainWindow = null;
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
+initApp(mainWindow, configWindow);
 
-var mainWindow = null;
-
-app.on('ready', function() {
-    mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800
+// Control your application’s event lifecycle.
+function initApp(mwindow, callback) {
+    app.on('ready', function() {
+        callback(mwindow);
     });
+    app.on('window-all-closed', () => {
+        app.quit();
+    });
+    app.on('activate', () => {
+        if (mwindow === null) {
+            callback(mwindow);
+        }
+    });
+}
 
-    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
-});
+// Create and control browser windows.
+function configWindow(window) {
+    window = new BrowserWindow({
+        frame: false,
+        height: 700,
+        width: 368,
+        backgroundColor: '#2e2c29',
+        show: false,
+        resizable: false
+    });
+    // Load local html file to browser window
+    window.loadURL(url.format({
+        pathname: path.join(__dirname, '/app/index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    // wait untill when rendered process has done
+    window.once('ready-to-show', () => {
+        window.show();
+    });
+    // open DevTools
+    // window.webContents.openDevTools();
+
+    // Emmited when the window is closed.
+    window.on('closed', () => {
+        window = null;
+    });
+}
