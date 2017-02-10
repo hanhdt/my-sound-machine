aa61f56fad3c4141d2485497544c61953ae632f2
Hanh Tran <tranduchanh.ms@gmail.com>
2017-02-10 15:37:17 +0900

    Query select the sound buttons; Iterate through the buttons reading out the data-sound attribute; Add a background image to each button; Add a click event to each button that plays audio

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,27 @@
-// Add your index.js code in this file
+'use strict';
+const path = require('path');
+let soundButtons = document.querySelectorAll('.button-sound');
+let closeButton = document.getElementsByClassName('.close');
+let settingButton = document.getElementsByClassName('.settings');
+
+setUpSoundButtons(soundButtons, prepareSoundButton);
+
+function setUpSoundButtons(buttons, callback) {
+    for (var i = 0; i < buttons.length; i++) {
+        let soundButton = buttons[i];
+        let soundName = soundButton.attributes['data-sound'].value;
+
+        callback(soundButton, soundName);
+    }
+}
+
+function prepareSoundButton(sButton, sName) {
+    sButton.querySelector('span').style.backgroundImage = 'url("img/icons/' + sName + '.png")';
+
+    let audio = new Audio(path.join(__dirname, '/wav/' + sName + '.wav'));
+
+    sButton.addEventListener('click', function() {
+        audio.currentTime = 0;
+        audio.play();
+    });
+}
